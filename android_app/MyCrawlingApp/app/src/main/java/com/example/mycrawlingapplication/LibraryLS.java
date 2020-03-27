23d3fd7268a99518c5cb596b719da95b99dcfacf
dongseoki <43495319+dongseoki@users.noreply.github.com>
2020-03-27 21:07:52 +0900

    상명대 크롤링 성공

---
+++ android_app/MyCrawlingApp/app/src/main/java/com/example/mycrawlingapplication/LibraryLS.java
@@ -0,0 +1,9 @@
+package com.example.mycrawlingapplication;
+
+import java.util.ArrayList;
+
+public class LibraryLS {
+    String sid;
+    ArrayList<LoanStatus> loanStatusList;
+    String errorMessage;
+}
---
+++ android_app/MyCrawlingApp/app/src/main/java/com/example/mycrawlingapplication/LoanStatus.java
@@ -0,0 +1,12 @@
+package com.example.mycrawlingapplication;
+
+public class LoanStatus {
+    String title;
+    String RN;
+    String CN;
+    String POS;
+    int STATE;
+    String RDD;
+    int BN;
+    String errorMessage;
+}
